d180e299e2ab9a323ac634ed8f08bd6bfc7ebf56
bijiwei1 <bibijijiweihch@gmail.com>
2018-11-27 20:01:21 -0800

    Read clauses

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,153 +1,89 @@
-/*****************************************************************************************[Main.cc]
-Copyright (c) 2003-2006, Niklas Een, Niklas Sorensson
-Copyright (c) 2007-2010, Niklas Sorensson
+#include <cmath>
+#include <vector>
+#include "xcl2.hpp"
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
-associated documentation files (the "Software"), to deal in the Software without restriction,
-including without limitation the rights to use, copy, modify, merge, publish, distribute,
-sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or
-substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
-NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT
-OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-**************************************************************************************************/
-
-#include <errno.h>
-#include <zlib.h>
-
-#include "minisat/utils/System.h"
-#include "minisat/utils/ParseUtils.h"
-#include "minisat/utils/Options.h"
-#include "minisat/core/Dimacs.h"
-#include "minisat/core/Solver.h"
-
-using namespace Minisat;
-
-//=================================================================================================
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
 
 
-static Solver* solver;
-// Terminate by notifying the solver and back out gracefully. This is mainly to have a test-case
-// for this feature of the Solver as it may take longer than an immediate call to '_exit()'.
-static void SIGINT_interrupt(int) { solver->interrupt(); }
+#define NUM_CLAUSES 1065
+#define NUM_TEST 1
 
-// Note that '_exit()' rather than 'exit()' has to be used. The reason is that 'exit()' calls
-// destructors and may cause deadlocks if a malloc/free function happens to be running (these
-// functions are guarded by locks for multithreaded use).
-static void SIGINT_exit(int) {
-    printf("\n"); printf("*** INTERRUPTED ***\n");
-    if (solver->verbosity > 0){
-        solver->printStats();
-        printf("\n"); printf("*** INTERRUPTED ***\n"); }
-    _exit(1); }
-
-
-//=================================================================================================
-// Main:
+// Util functions for host
+void read_clause_file(string filename,
+    vector<int, aligned_allocator<int>> &c1,
+    vector<int, aligned_allocator<int>> &c2,
+    vector<int, aligned_allocator<int>> &c3);
 
 
 int main(int argc, char** argv)
 {
-    try {
-        setUsageHelp("USAGE: %s [options] <input-file> <result-output-file>\n\n  where input may be either in plain or gzipped DIMACS.\n");
-        setX86FPUPrecision();
+  //Allocate Memory in Host Memory
+  size_t vector_size_bytes = sizeof(int) * NUM_CLAUSES;
 
-        // Extra options:
-        //
-        IntOption    verb   ("MAIN", "verb",   "Verbosity level (0=silent, 1=some, 2=more).", 1, IntRange(0, 2));
-        IntOption    cpu_lim("MAIN", "cpu-lim","Limit on CPU time allowed in seconds.\n", 0, IntRange(0, INT32_MAX));
-        IntOption    mem_lim("MAIN", "mem-lim","Limit on memory usage in megabytes.\n", 0, IntRange(0, INT32_MAX));
-        BoolOption   strictp("MAIN", "strict", "Validate DIMACS header during parsing.", false);
-        
-        parseOptions(argc, argv, true);
+  vector<int, aligned_allocator<>> source_in1(NUM_CLAUSES);
+  vector<int, aligned_allocator<int>> source_in2(NUM_CLAUSES);
+  vector<int, aligned_allocator<int>> source_in3(NUM_CLAUSES);
 
-        Solver S;
-        double initial_time = cpuTime();
+  vector<int, aligned_allocator<int>> hw_results(1);
 
-        S.verbosity = verb;
-        
-        solver = &S;
-        // Use signal handlers that forcibly quit until the solver will be able to respond to
-        // interrupts:
-        sigTerm(SIGINT_exit);
+  read_clause_file("../data/uf250-01.cnf", source_in1,source_in2,source_in3);
+  hw_results[0] = 0;
 
-        // Try to set resource limits:
-        if (cpu_lim != 0) limitTime(cpu_lim);
-        if (mem_lim != 0) limitMemory(mem_lim);
-        
-        if (argc == 1)
-            printf("Reading from standard input... Use '--help' for help.\n");
-        
-        gzFile in = (argc == 1) ? gzdopen(0, "rb") : gzopen(argv[1], "rb");
-        if (in == NULL)
-            printf("ERROR! Could not open file: %s\n", argc == 1 ? "<stdin>" : argv[1]), exit(1);
-        
-        if (S.verbosity > 0){
-            printf("============================[ Problem Statistics ]=============================\n");
-            printf("|                                                                             |\n"); }
-        
-        parse_DIMACS(in, S, (bool)strictp);
-        gzclose(in);
-        FILE* res = (argc >= 3) ? fopen(argv[2], "wb") : NULL;
-        
-        if (S.verbosity > 0){
-            printf("|  Number of variables:  %12d                                         |\n", S.nVars());
-            printf("|  Number of clauses:    %12d                                         |\n", S.nClauses()); }
-        
-        double parsed_time = cpuTime();
-        if (S.verbosity > 0){
-            printf("|  Parse time:           %12.2f s                                       |\n", parsed_time - initial_time);
-            printf("|                                                                             |\n"); }
- 
-        // Change to signal-handlers that will only notify the solver and allow it to terminate
-        // voluntarily:
-        sigTerm(SIGINT_interrupt);
-       
-        if (!S.simplify()){
-            if (res != NULL) fprintf(res, "UNSAT\n"), fclose(res);
-            if (S.verbosity > 0){
-                printf("===============================================================================\n");
-                printf("Solved by unit propagation\n");
-                S.printStats();
-                printf("\n"); }
-            printf("UNSATISFIABLE\n");
-            exit(20);
-        }
-        
-        vec<Lit> dummy;
-        lbool ret = S.solveLimited(dummy);
-        if (S.verbosity > 0){
-            S.printStats();
-            printf("\n"); }
-        printf(ret == l_True ? "SATISFIABLE\n" : ret == l_False ? "UNSATISFIABLE\n" : "INDETERMINATE\n");
-        if (res != NULL){
-            if (ret == l_True){
-                fprintf(res, "SAT\n");
-                for (int i = 0; i < S.nVars(); i++)
-                    if (S.model[i] != l_Undef)
-                        fprintf(res, "%s%s%d", (i==0)?"":" ", (S.model[i]==l_True)?"":"-", i+1);
-                fprintf(res, " 0\n");
-            }else if (ret == l_False)
-                fprintf(res, "UNSAT\n");
-            else
-                fprintf(res, "INDET\n");
-            fclose(res);
-        }
-        
-#ifdef NDEBUG
-        exit(ret == l_True ? 10 : ret == l_False ? 20 : 0);     // (faster than "return", which will invoke the destructor for 'Solver')
-#else
-        return (ret == l_True ? 10 : ret == l_False ? 20 : 0);
-#endif
-    } catch (OutOfMemoryException&){
-        printf("===============================================================================\n");
-        printf("INDETERMINATE\n");
-        exit(0);
+
+//OPENCL HOST CODE AREA START
+  vector<cl::Device> devices = xcl::get_xil_devices();
+  cl::Device device = devices[0];
+
+  cl::Context context(device);
+  cl::CommandQueue q(context, device, CL_QUEUE_PROFILING_ENABLE);
+  string device_name = device.getInfo<CL_DEVICE_NAME>(); 
+
+  string binaryFile = xcl::find_binary_file(device_name, "dot_prod_kernel");
+  cl::Program::Binaries bins = xcl::import_binary_file(binaryFile);
+  devices.resize(1);
+  cl::Program program(context, devices, bins);
+  cl::Kernel kernel(program, "dot_prod_kernel");
+
+  //Allocate Buffer in Global Memory
+  vector<cl::Memory> inBufVec, outBufVec;
+  cl::Buffer buffer_in1(context,CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
+                        vector_size_bytes, source_in1.data());
+  cl::Buffer buffer_in2(context,CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY, 
+                        vector_size_bytes, source_in2.data());
+  cl::Buffer buffer_in2(context,CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY, 
+                        vector_size_bytes, source_in3.data());
+
+  cl::Buffer buffer_output(context,CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY, 
+                           sizeof(int), hw_results.data());
+
+  inBufVec.push_back(buffer_in1);
+  inBufVec.push_back(buffer_in2);
+  inBufVec.push_back(buffer_in3);
+  outBufVec.push_back(buffer_output);
+
+  //Copy input data to device global memory
+  q.enqueueMigrateMemObjects(inBufVec, 0/* 0 means from host*/);
+
+  auto krnl_solver = cl::KernelFunctor<cl::Buffer&, cl::Buffer&,
+                                           cl::Buffer&, cl::Buffer&>(kernel);
+
+  //Launch the Kernel
+  NUM_NON_SAT = 0; 
+  for (int i = 0; i < NUM_TEST; ++i) {
+    krnl_solver(cl::EnqueueArgs(q, cl::NDRange(1, 1, 1), cl::NDRange(1, 1, 1)),cl::NDRange(1, 1, 1),
+                  buffer_in1, buffer_in2, buffer_in3, buffer_output);
+
+    //Copy Result from Device Global Memory to Host Local Memory
+    q.enqueueMigrateMemObjects(outBufVec, CL_MIGRATE_MEM_OBJECT_HOST);
+    q.finish();
+    if (hw_results != 1){
+        NUM_NON_SAT ++;
     }
+  }
+//OPENCL HOST CODE AREA END
+  print ("Unsatisified cases: " + NUM_NON_SAT); 
+
 }
--- src/util.cpp
+++ src/util.cpp
@@ -16,9 +16,11 @@ using std::ios;
 using std::string;
 using std::sort;
 
-void read_train_file(string filename,
-    vector<unsigned long, aligned_allocator<unsigned long>> &train_image,
-    int offset) {
+void read_clause_file(string filename,
+    vector<int, aligned_allocator<int>> &c1,
+    vector<int, aligned_allocator<int>> &c2,
+    vector<int, aligned_allocator<int>> &c3) { 
+
   ifstream f;
   f.open(filename.c_str(), ios::in);
   if (!f.is_open()) {
@@ -30,64 +32,21 @@ void read_train_file(string filename,
   while (!f.eof()) {
     string str;
     f >> str;
-    unsigned long val = strtoul(str.substr(0, str.length() - 1).c_str(),
-        NULL, 0);
-    train_image[offset + cnt++] = val;
-  }
-  f.close();
-  return ;
-}
-
-void read_test_file(string filename,
-    vector<unsigned long, aligned_allocator<unsigned long>> &test_image,
-    vector<int, aligned_allocator<int>> &test_label) {
-
-  ifstream f;
-  f.open(filename.c_str(), ios::in);
-  if (!f.is_open()) {
-    cout << "Open " << filename << " failed" << endl;
-    exit(1);
-  }
-  int cnt = 0;
-  while (!f.eof()) {
-    string str;
-    f >> str;
-    unsigned long val = strtoul(str.substr(0, str.length() - 2).c_str(),
-        NULL, 0);
-    test_image[cnt] = val;
-    int label = str[str.length() - 1] - '0';
-    test_label[cnt++] = label;
-  }
-  f.close();
-  return ;
-}
-
-int vote(vector<unsigned char, aligned_allocator<unsigned char>> &knn_mat) {
-  int score[10] = {0};
-
-  for (int i = 0; i < 30; i += 3)
-    sort(knn_mat.begin() + i, knn_mat.begin() + i + 3);
-
-  for (int i = 0; i < 3; ++i) {
-    int m = INT_MAX;
-    int id = 0;
-    for (int j = 0; j < 10; ++j) {
-      if (knn_mat[j * 3 + i] < m) {
-        m = knn_mat[j * 3 + i];
-        id = j;
+    if str.at(0) != 'p' and str.at(0) != 'c' {
+      substrs = strtok(str, " ");
+      if substrs[0] == ""{
+        c1[cnt] = atoi(substrs[1]);
+        c2[cnt] = atoi(substrs[2]);
+        c3[cnt] = atoi(substrs[3]);
+      }else{
+        c1[cnt] = atoi(substrs[0]);
+        c2[cnt] = atoi(substrs[1]);
+        c3[cnt] = atoi(substrs[2]);
       }
-    }
-    score[id]++;
-  }
- 
-  int vid = 0;
-  int vm = -1;
-  for (int i = 0; i < 10; ++i) {
-    if (score[i] > vm) {
-      vm = score[i];
-      vid = i;
+      cnt ++; 
     }
   }
-  return vid;
-}
 
+  f.close();
+  return ;
+}
