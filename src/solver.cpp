028c09b8ad6119849a135de781339893e5904324
Jiwei Bi <bibijijiweihch@gmail.com>
2018-11-30 16:15:27 -0800

    update

--- src/solver.cpp
+++ src/solver.cpp
@@ -21,7 +21,7 @@ void solver_kernel(
   int c3_local[NUM_CLAUSES];
   int tmp[NUM_CLAUSES];
 
-  //#pragma ACCEL pipeline flatten
+  #pragma ACCEL pipeline 
   for (int x = 0; x < NUM_CLAUSES; ++x) {
     c1_local[x] = c1[x]; 
     c2_local[x] = c2[x]; 
@@ -33,49 +33,11 @@ void solver_kernel(
     tmp[x] = c1_local[x] + c2_local[x] +c3_local[x];
   }
 
+  /*
+  while(){
+
+  }*/
+
   result =tmp[0]; 
 
-/*
-  while (){
-
-  }
-*/
-
-/*
-    unsigned char buf_knn_mat[10][3];
-
-    for (int x = 0; x < 10; ++x) {
-        for (int y = 0; y < 3; ++y) {
-            buf_knn_mat[x][y] = 50;
-        }
-    }
-
-    for (int x = 0; x < 10; ++x) {
-        for (int y = 0; y < 1800; ++y) {
-            int temp = train_images[x * 1800 + y] ^ test_image;
-            unsigned char dis = 0;
-            for (int i = 0; i < 49; ++i) {
-                dis += (temp & (1L << i)) >> i;
-            }
-
-            unsigned char max_id = 0;
-            unsigned char max_val = 0;
-            for (int i1 = 0; i1 < 3; ++i1) {
-                if (max_val < buf_knn_mat[x][i1]) {
-                    max_id = i1;
-                    max_val = buf_knn_mat[x][i1];
-                }
-            }
-            if (dis < max_val) {
-                buf_knn_mat[x][max_id] = dis;
-            }
-        }
-    }
-
-    for (int x = 0; x < 10; ++x) {
-        for (int y = 0; y < 3; ++y) {
-            knn_mat[x * 3 + y] = buf_knn_mat[x][y];
-        }
-    }
-    */
 }
