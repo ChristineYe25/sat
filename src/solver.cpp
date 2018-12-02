49f3bc52e9997d9ae7c2c2bf409eab10f5d1feb1
bijiwei1 <bibijijiweihch@gmail.com>
2018-12-02 12:01:10 -0800

    update

--- src/solver.cpp
+++ src/solver.cpp
@@ -25,7 +25,7 @@ using namespace std;
 void collect_buffer(int pos_cls[NUM_VARS][BUF_SIZE], int neg_cls[NUM_VARS][BUF_SIZE], 
   const int var, const int x){
    if (var> 0){
-      assert(pos_cls[var][4]>0);
+      //assert(pos_cls[var][4]>0);
       if (pos_cls[var][0] == 0){
         pos_cls[var][0] = x; 
       }else if (pos_cls[var][1] == 0){
@@ -36,9 +36,10 @@ void collect_buffer(int pos_cls[NUM_VARS][BUF_SIZE], int neg_cls[NUM_VARS][BUF_S
         pos_cls[var][3] = x; 
       }else {
         pos_cls[var][4] = x; 
+        printf("Get here "); 
       }
     }else{
-      assert(neg_cls[var][4]>0);
+      //assert(neg_cls[var][4]>0);
       if (neg_cls[-var][0] == 0){
         neg_cls[-var][0] = x; 
       }else if (neg_cls[-var][1] == 0){
@@ -49,6 +50,7 @@ void collect_buffer(int pos_cls[NUM_VARS][BUF_SIZE], int neg_cls[NUM_VARS][BUF_S
         neg_cls[-var][3] = x; 
       }else { 
         neg_cls[-var][4] = x; 
+        printf("Get here 2"); 
       }
     }
 }
