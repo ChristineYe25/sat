7b3736a4d7323c1ec74a9ce82060585419d2d933
bijiwei1 <bibijijiweihch@gmail.com>
2018-12-02 11:52:24 -0800

    update

--- src/solver.cpp
+++ src/solver.cpp
@@ -2,7 +2,7 @@
 #include <math.h>
 #include <assert.h>
 #include <stdlib.h>
-#include <string> 
+//#include <string> 
 
 #define NUM_CLAUSES 1065
 #define NUM_VARS 250 
@@ -173,7 +173,7 @@ void solver_kernel(
         assigned_vars_stack[stack_end_ptr] = new_var_idx;
 
         state = PROP;
-        printf("Decide var : v" + std::to_string(new_var_idx) +"\n");
+        printf("Decide var : v" +"\n");
 
       case PROP:
         bool conflict[BUF_SIZE];
